111aacc6ded182e692e62bef1861eecf7bf4380f
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-03-07 18:38:44 +0200

    new repo

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+let form=document.getElementById('form');
+let table=document.getElementById('table');
+
+
+
+function CarGarage(name,catigory,model){
+    this.name;
+    this.catigory;
+    this.model;
+
+}
+
+CarGarage.prototype.gitInputs=function(){
+
+}
